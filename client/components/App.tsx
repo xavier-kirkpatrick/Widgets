ae670026542d40402b2a9b44b6ad9e5dfec1cf98
Xavier <143090232+xavier-kirkpatrick@users.noreply.github.com>
2023-11-08 10:20:47 +1300

    add log for usualstate check data shape

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,4 +1,13 @@
+import { useState, useEffect } from 'react'
+import { Widget } from '../../models/Widget'
+
 function App() {
+  const [usualState, setUsualState] = useState([] as Widget[])
+
+  useEffect(() => {
+    setUsualState(console.log('using the effect'))
+  }, [])
+
   return (
     <div>
       <h1>Widgets for the win!</h1>
